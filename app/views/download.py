6f65545e0c74587dc05d28b7b7ad44029d259775
Dev Goyal <65810521+dgbkn@users.noreply.github.com>
2024-01-25 21:40:19 +0530

    range stream

--- app/views/download.py
+++ app/views/download.py
@@ -1,14 +1,18 @@
 import logging
+
 from aiohttp import web
 from telethon.tl.custom import Message
+
 from app.util import get_file_name
 from app.config import block_downloads
 from .base import BaseView
 
+
 log = logging.getLogger(__name__)
 
+
 class Download(BaseView):
-    async def download_get(self, req: web.Request) -> web.StreamResponse:
+    async def download_get(self, req: web.Request) -> web.Response:
         return await self.handle_request(req)
 
     async def download_head(self, req: web.Request) -> web.Response:
@@ -16,9 +20,9 @@ class Download(BaseView):
 
     async def handle_request(
         self, req: web.Request, head: bool = False
-    ) -> web.StreamResponse:
+    ) -> web.Response:
         if block_downloads:
-            return web.Response(status=403, text="403: Forbidden" if not head else None)
+            return web.Response(status=403, text="403: Forbiden" if not head else None)
 
         file_id = int(req.match_info["id"])
         alias_id = req.match_info["chat"]
@@ -34,7 +38,7 @@ class Download(BaseView):
             message = None
 
         if not message or not message.file:
-            log.debug(f"No result for {file_id} in {chat_id}")
+            log.debug(f"no result for {file_id} in {chat_id}")
             return web.Response(
                 status=410,
                 text="410: Gone. Access to the target resource is no longer available!"
@@ -44,6 +48,7 @@ class Download(BaseView):
 
         media = message.media
         size = message.file.size
+        file_size = size
         file_name = get_file_name(message, quote_name=False)
         mime_type = message.file.mime_type
 
@@ -51,46 +56,71 @@ class Download(BaseView):
             request = req
             range_header = request.headers.get('Range', 0)
             if range_header:
-                range_data = range_header.replace('bytes=', '').split('-')
-                from_bytes = int(range_data[0])
-                until_bytes = int(range_data[1]) if range_data[1] else size - 1
+              range_data = range_header.replace('bytes=', '').split('-')
+              from_bytes = int(range_data[0])
+              until_bytes = int(range_data[1]) if range_data[1] else file_size - 1
             else:
-                from_bytes = request.http_range.start or 0
-                until_bytes = request.http_range.stop or size - 1
+              from_bytes = request.http_range.start or 0
+              until_bytes = request.http_range.stop or file_size - 1
 
             req_length = until_bytes - from_bytes
 
-            if (until_bytes > size) or (from_bytes < 0) or (until_bytes < from_bytes):
-                raise ValueError("Range not in acceptable format")
+
+            # offset = req.http_range.start or 0
+            # limit = req.http_range.stop or size
+            offset = from_bytes or 0
+            limit = until_bytes or size            
+            if (limit > size) or (offset < 0) or (limit < offset):
+                raise ValueError("range not in acceptable format")
         except ValueError:
             return web.Response(
                 status=416,
                 text="416: Range Not Satisfiable",
-                headers={
+                headers = {
                     "Content-Range": f"bytes */{size}"
                 }
             )
 
         if not head:
-            # Use download method for streaming
-            body = self.client.download(media, size, from_bytes, until_bytes)
-            log.info(f"Serving file in {message.id} (chat {chat_id}) ; Range: {from_bytes} - {until_bytes}")
+            body = self.client.download(media, size, offset, limit)
+            log.info(f"Serving file in {message.id} (chat {chat_id}) ; Range: {offset} - {limit}")
         else:
             body = None
 
-        response = web.StreamResponse(
-            status=206 if req.http_range.start else 200,
-            headers={
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": mime_type,
-                "Content-Range": f"bytes {from_bytes}-{until_bytes}/{size}",
-                "Content-Disposition": f'attachment; filename="{file_name}"',
-                "Accept-Ranges": "bytes",
-            }
+        # headers={
+        #     "Connection":"keep-alive",
+        #     "Content-Type": mime_type,
+        #     "Content-Range": f"bytes {offset}-{limit}/{size}",
+        #     "Accept-Ranges": "bytes",
+        #     "Content-Disposition": f'attachment; filename="{file_name}"',
+        #     "Transfer-Encoding":"chunked",
+        #    #"Content-Length": str(limit - offset)
+
+        # }
+
+        # r=  web.Response(
+        #     status=206 if req.http_range.start else 200,
+        #     body=body,
+        #     headers=headers
+        # )
+        
+        # r.enable_chunked_encoding()
+        # return r
+        
+        return_resp = web.Response(
+        status=206 if req.http_range.start else 200,
+        body=body,
+        headers={
+          "Access-Control-Allow-Origin": "*",
+            "Content-Type": mime_type,
+            "Content-Range": f"bytes {offset}-{limit}/{size}",
+            "Content-Disposition": f'attachment; filename="{file_name}"',
+            "Accept-Ranges": "bytes",
+            # "Content-Length": f"{limit-offset}"
+           }
         )
-        await response.prepare(req)
 
-        if not head:
-            await response.write(body)
+        if return_resp.status == 200:
+          return_resp.headers.add("Content-Length", str(size))
 
-        return response
+        return return_resp
